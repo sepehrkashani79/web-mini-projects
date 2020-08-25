afca3fa4d659fc165d8fa694a31ef63732612008
sepehrkashani79 <sepehrkashani79@gamil.com>
2020-08-25 19:23:19 +0430

    bg transition - main button- sub buttons added

---
+++ pageMan.js
@@ -0,0 +1,8 @@
+function startBackgroundTransition(){
+    // document.body.style.transition="2s"
+    // document.body.style.background="linear-gradient(0deg, rgba(24,40,72,1) 0%, rgba(75,108,183,1) 100%)"
+    // document.body.style.backgroundRepeat="no-repeat"
+    // document.body.style.backgroundAttachment = "fixed"
+    document.getElementById("backgroundDiv").style.opacity="100%"
+    // console.log("page loaded");
+}
